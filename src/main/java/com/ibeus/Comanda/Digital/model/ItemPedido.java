c2ef6440cf82953a9ebc163beff890338d2b3b2e
JoãoMizael <joaomizael3712@gmail.com>
2024-10-18 18:10:29 -0300

    criação do ItemPedido e ItemPedidoRepository para conexão de Dish com Pedido usando ItemPedido como intermediario

--- src/main/java/com/ibeus/Comanda/Digital/model/ItemPedido.java
+++ src/main/java/com/ibeus/Comanda/Digital/model/ItemPedido.java
@@ -1,4 +1,24 @@
 package com.ibeus.Comanda.Digital.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "itempedido")
+@Data
 public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedidos_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "dishes_id")
+    private Dish dish;
+
+    private int quantidade;
 }
--- src/main/java/com/ibeus/Comanda/Digital/repository/ItemPedidoRepository.java
+++ src/main/java/com/ibeus/Comanda/Digital/repository/ItemPedidoRepository.java
@@ -1,4 +1,7 @@
 package com.ibeus.Comanda.Digital.repository;
 
-public interface ItemPedidoRepository {
+import com.ibeus.Comanda.Digital.model.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
 }
