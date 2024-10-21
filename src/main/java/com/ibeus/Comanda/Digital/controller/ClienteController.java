855be7d3f6dce30d32a41292cd15a5124f64727d
Pedro Tezza <pdtezza@gmail.com>
2024-10-21 14:31:56 -0300

    criacao da ClienteService.java

---
+++ src/main/java/com/ibeus/Comanda/Digital/controller/ClienteController.java
@@ -0,0 +1,57 @@
+package com.ibeus.Comanda.Digital.controller;
+
+import java.util.List;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibeus.Comanda.Digital.model.Cliente;
+import com.ibeus.Comanda.Digital.model.Pedido;
+import com.ibeus.Comanda.Digital.service.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;      
+   
+    @PostMapping
+    public Cliente criarCliente(@RequestBody Cliente cliente) {
+        return clienteService.criarCliente(cliente);
+    }
+
+    @GetMapping
+    public List<Cliente> listarClientes() {
+        return clienteService.listarClientes();
+    }
+
+    // Acompanhar um pedido
+     @GetMapping("/acompanhar-pedido/{pedidoId}")
+    public String acompanharPedido(@PathVariable Long pedidoId) {
+        return clienteService.acompanharPedido(pedidoId);
+    }
+    @PutMapping("/avancar-status/{pedidoId}")
+    public String avancarStatusPedido(@PathVariable Long pedidoId) {
+        return clienteService.avancarStatusPedido(pedidoId);
+    }
+
+    // Retroceder status do pedido (Restaurante)
+    @PutMapping("/retroceder-status/{pedidoId}")
+    public String retrocederStatusPedido(@PathVariable Long pedidoId) {
+        return clienteService.retrocederStatusPedido(pedidoId);
+    }
+
+    // Listar todos os pedidos simulados (para fins de teste)
+    @GetMapping("/listar-pedidos")
+    public Map<Long, Pedido> listarPedidos() {
+        return clienteService.listarPedidosSimulados();
+    }
+}
---
+++ src/main/java/com/ibeus/Comanda/Digital/service/ClienteService.java
@@ -0,0 +1,77 @@
+package com.ibeus.Comanda.Digital.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ibeus.Comanda.Digital.model.Cliente;
+import com.ibeus.Comanda.Digital.model.Pedido;
+import com.ibeus.Comanda.Digital.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    private final Map<Long, Pedido> pedidosSimulados = new HashMap<>();
+    
+    public ClienteService() {
+        
+        pedidosSimulados.put(1L, new Pedido(1L, "Pizza"));
+        pedidosSimulados.put(2L, new Pedido(2L, "Hamburguer"));
+        pedidosSimulados.put(3L, new Pedido(3L, "Batata"));
+    }
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente criarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+
+    public List<Cliente> listarClientes() {
+        return clienteRepository.findAll();
+    }
+
+
+    public String acompanharPedido(Long pedidoId) {
+        Pedido pedido = pedidosSimulados.get(pedidoId);
+        if (pedido != null) {
+            return "Pedido " + pedidoId + ": " + pedido.getDescricao() + ", Status: " + pedido.getStatus();
+        } else {
+            return "Pedido não encontrado.";
+        }
+    }
+
+    // Avançar status do pedido (restaurante)
+    public String avancarStatusPedido(Long pedidoId) {
+        Pedido pedido = pedidosSimulados.get(pedidoId);
+        if (pedido != null && !pedido.isFinalizado()) {
+            pedido.avancarStatus();
+            return "Status do pedido " + pedidoId + " atualizado para: " + pedido.getStatus();
+        } else if (pedido.isFinalizado()) {
+            return "O pedido já foi entregue.";
+        } else {
+            return "Pedido não encontrado.";
+        }
+    }
+
+    // Retroceder status do pedido (restaurante)
+    public String retrocederStatusPedido(Long pedidoId) {
+        Pedido pedido = pedidosSimulados.get(pedidoId);
+        if (pedido != null && !pedido.isRecebido()) {
+            pedido.retrocederStatus();
+            return "Status do pedido " + pedidoId + " atualizado para: " + pedido.getStatus();
+        } else if (pedido.isRecebido()) {
+            return "O pedido está no status inicial e não pode ser retrocedido.";
+        } else {
+            return "Pedido não encontrado.";
+        }
+    }
+
+    // Listar todos os pedidos simulados (para fins de teste)
+    public Map<Long, Pedido> listarPedidosSimulados() {
+        return pedidosSimulados;
+    }
+ }
